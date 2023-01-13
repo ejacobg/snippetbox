99d9482bf9c190b1276ec8a0e19e95eeabbf85b3
Ean Jacob Gayban <ejacobg@outlook.com>
2023-01-13 12:34:16 -0800

    Finished 2.4

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+func main() {
+
+}
