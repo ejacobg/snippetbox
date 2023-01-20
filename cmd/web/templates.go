bab8bceccc8b6f242ac5b762da4614be15f1b7d3
Ean Jacob Gayban <ejacobg@outlook.com>
2023-01-20 13:29:19 -0800

    Finished 5.6, using humanDate function to render dates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,12 +4,21 @@ import (
 	"github.com/ejacobg/snippetbox/internal/models"
 	"html/template"
 	"path/filepath"
+	"time"
 )
 
 type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (cache map[string]*template.Template, err error) {
@@ -23,7 +32,7 @@ func newTemplateCache() (cache map[string]*template.Template, err error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.go.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.go.html")
 		if err != nil {
 			return nil, err
 		}
